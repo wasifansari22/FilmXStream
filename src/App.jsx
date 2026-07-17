330cf6967f124f8dd90dff91152c1f1b67d2f966
Wasif <ansari22wasif@gmail.com>
2026-07-17 21:04:46 +0530

    Configure Tailwind CSS

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import React from "react"
 function App() {
 
   return (
-    <>
-      <h1>Netflix</h1>
-    </>
+    <div className="min-h-screen flex items-center justify-center bg-black">
+      <h1 className="text-5xl font-bold text-red-600">NetflixX</h1>
+    </div>
   )
 }
 
