ac451359ee86d6dd7953079eb8c059c889270373
Wasif <ansari22wasif@gmail.com>
2026-07-18 13:37:47 +0530

    Enhance landing page hero section and Watch later conv to button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,21 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { FaHeart } from "react-icons/fa";
 
 const Header = () => {
     return (
         <header className="absolute top-0 left-0 w-full z-50">
             <div className="max-w-7xl mx-auto px-6 py-5 flex justify-between items-center">
-                <h1 className="text-red-600 text-4xl font-bold tracking-wide">
-                    NetflixX
+                <h1 className="text-red-600 text-3xl md:text-4xl font-bold tracking-wide">
+                    NetflixX    
                 </h1>
 
-                <div className="flex gap-4">
+                <div className="flex gap-8">
 
                     <Link
                         to="/watchlater"
-                        className="text-white hover:text-red-500 transition"
-                    >
+                        className="flex items-center gap-2 border border-red-600 px-5 py-2 rounded-md text-red-500 hover:bg-red-600 hover:text-white transition-all duration-300"
+                    >  <FaHeart />
                         Watch Later
                     </Link>
                 </div>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,25 +1,26 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import heroImage from '../assets/images/hero.jpg'
 
 const Hero = () => {
     return (
         <>
-            <section className="relative h-screen bg-black">
-                <div className="absolute inset-0 bg-black/70"></div>
+            <section className="relative h-screen bg-cover bg-center" style={{ backgroundImage: `url(${heroImage})` }}>
+                <div className="absolute inset-0 bg-linear-to-r from-black via-black/70 to-black/30"></div>
                 <div className="relative z-10 flex flex-col items-center justify-center h-full text-center px-6">
-                    <h1 className="text-5xl md:text-7xl font-bold">
+                    <h1 className="text-5xl md:text-7xl lg:text-8xl font-bold">
                         Unlimited Movies,
                         <br />
                         TV Shows and More
                     </h1>
 
-                    <p className="mt-6 text-lg md:text-2xl text-gray-300">
+                    <p className="mt-6 md:text-2xl max-w-2xl text-gray-300">
                         Watch anywhere. Cancel anytime.
                     </p>
 
                     <Link
                         to="/movies"
-                        className="mt-10 bg-red-600 hover:bg-red-700 transition px-8 py-4 rounded-lg text-lg font-semibold"
+                        className="mt-10 bg-red-600 hover:bg-red-700 hover:scale-105 transition-all duration-300 px-8 py-4 rounded-lg text-lg font-semibold shadow-xl"
                     >
                         Start Watching
                     </Link>
