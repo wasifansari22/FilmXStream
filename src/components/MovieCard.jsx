4f8762bbbbe71f8897c08f79ca5318811a925fb4
Wasif <ansari22wasif@gmail.com>
2026-07-19 16:51:37 +0530

    Implement Watch Later Toggle with Redux

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,7 +1,27 @@
 import React from 'react';
 import { imageBaseUrl } from '../services/tmdbApi';
+import { useDispatch, useSelector } from 'react-redux';
+import { addMovie, removeMovie } from '../redux/slices/watchLaterSlice';
+import { FaHeart, FaRegHeart } from "react-icons/fa";
 
 const MovieCard = ({ movie }) => {
+    // change redux state
+    const dispatch = useDispatch();
+    // read redux state
+    const savedMovies = useSelector((state) => state.watchLater.movies);
+
+    const isSaved = savedMovies.some(
+        (savedMovies) => savedMovies.id === movie.id
+    );
+
+    const handleWatchLater = () => {
+        if (isSaved) {
+            dispatch(removeMovie(movie.id));
+        } else {
+            dispatch(addMovie(movie));
+        }
+    };
+
     return (
         <div className="bg-[#1f1f1f] rounded-lg overflow-hidden shadow-lg hover:scale-105 transition-all duration-300">
             <img
@@ -17,6 +37,16 @@ const MovieCard = ({ movie }) => {
                 <p className="mt-2 text-yellow-400">
                     ⭐ {movie.vote_average.toFixed(1)}
                 </p>
+
+                <button onClick={handleWatchLater}
+                    className={`mt-4 w-full flex justify-center items-center gap-2 py-2 rounded-lg transition-all duration-300 
+                ${isSaved ? "bg-red-600 hover:bg-red-700" : "border border-red-600 text-red-500 hover:bg-red-600 hover:text-white cursor-pointer"
+                        }`}
+                >
+                    {isSaved ? <FaHeart /> : <FaRegHeart />}
+                    {isSaved ? "Saved" : "Watch Later"}
+                </button>
+
             </div>
         </div>
     );
