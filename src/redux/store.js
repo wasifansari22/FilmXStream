626e12381c342b840d21a01ee98b1e25121bf19b
Wasif <ansari22wasif@gmail.com>
2026-07-22 12:47:11 +0530

    Movies saved into the LocalStorage - created Persistent Watch Later

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,47 @@
 import { configureStore } from "@reduxjs/toolkit";
 import watchLaterReducer from './slices/watchLaterSlice'
 
+// load saved movies from localStorage
+const loadWatchLater = () => {
+    try {
+        const savedData = localStorage.getItem("watchLater");
+
+        if (savedData === null) {
+            return undefined;
+        }
+
+        return {
+            watchLater: {
+                movies: JSON.parse(savedData),
+            },
+        };
+    } catch (error) {
+        console.error("Failed to load watch later:", error);
+        return undefined;
+    }
+};
+
+// save movies to localStorage
+const saveWatchLater = (state) => {
+    try {
+        localStorage.setItem(
+            "watchLater",
+            JSON.stringify(state.watchLater.movies)
+        );
+    } catch (error) {
+        console.error("Failed to save watch later:", error);
+    }
+};
+
 export const store = configureStore({
     reducer: {
         watchLater: watchLaterReducer,
     },
+    // preload the store
+    preloadedState: loadWatchLater(),
+});
+
+// subscribe() runs automatically -> saveWatchLater()
+store.subscribe(() => {
+    saveWatchLater(store.getState());
 });
