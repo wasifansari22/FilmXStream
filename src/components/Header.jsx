d4c6515b94496dc420e18d2d031c0714e293905c
Wasif <ansari22wasif@gmail.com>
2026-07-25 01:07:21 +0530

    Stick Header all pages

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { FaHeart } from "react-icons/fa";
 import { HiMenu, HiX } from "react-icons/hi";
@@ -6,13 +6,30 @@ import { useSelector } from 'react-redux';
 
 const Header = () => {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const [isScrolled, setIsScrolled] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            setIsScrolled(window.scrollY > 40);
+        };
+        window.addEventListener("scroll", handleScroll);
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, []);
+
     const savedMovies = useSelector((state) => state.watchLater.movies);
 
     return (
-        <header className="absolute top-0 left-0 w-full z-50">
+        <header
+            className={`fixed top-0 left-0 w-full z-50 transition-all duration-300 ${isScrolled
+                ? "bg-[#141414]/40 backdrop-blur-xl backdrop-saturate-150 shadow-lg"
+                : "bg-transparent"
+                }`}
+        >
             <div className="max-w-7xl mx-auto px-4 sm:px-6 py-4 flex justify-between items-center">
                 <Link to="/" >
-                    <h1 className="text-red-600 text-3xl sm:text-3xl md:text-4xl font-bold tracking-wide">
+                    <h1 className="text-red-600 text-2xl sm:text-3xl md:text-4xl font-bold tracking-wide">
                         FilmX<span className='text-white'>Stream</span>
                     </h1>
                 </Link>
@@ -21,7 +38,8 @@ const Header = () => {
 
                     <Link
                         to="/watchlater"
-                        className="relative flex items-center justify-center gap-2 border border-red-600 px-3 py-2 sm:px-5 sm:py-2 rounded-lg text-red-500 hover:bg-red-600 hover:text-white transition-all duration-300"
+                        // className="relative flex items-center justify-center gap-2 border border-red-600 px-3 py-2 sm:px-5 sm:py-2 rounded-lg text-red-500 hover:bg-red-600 hover:text-white transition-all duration-300"
+                        className="relative flex items-center gap-2 border border-red-600 px-3 sm:px-5 py-2 rounded-md text-sm sm:text-base text-red-500 hover:bg-red-600 hover:text-white transition-all duration-300"
                     >  <FaHeart />
                         {/* <span className="sm:hidden">Saved</span> */}
                         <span className='hidden sm:inline'>Watch Later</span>
