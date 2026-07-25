150edaf7246718fd23c52cb8f967e2b4ee3679b4
Wasif <ansari22wasif@gmail.com>
2026-07-25 20:54:06 +0530

    Removed all the comments

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
-// import Header from "../components/Header";
 import Hero from "../components/Hero";
-// import Footer from "../components/Footer";
 import Features from '../components/Features';
 import TrendingPreview from '../components/TrendingPreview';
 import Layout from '../components/Layout';
@@ -9,11 +7,9 @@ import Layout from '../components/Layout';
 const Landing = () => {
   return (
     <Layout>
-      {/* <Header /> */}
       <Hero />
       <Features />
       <TrendingPreview />
-      {/* <Footer /> */}
     </Layout>
   );
 };  
